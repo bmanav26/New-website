6d1725660ecea78baa0f6a865512800e606b33a1
Manav Bansal <manavbansal555@gmail.com>
2025-04-07 01:46:27 +0530

    Add React Router for Register and Login pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 
 function App() {
   return (
-    <div>
-      <Register />
-      <hr />
-      <Login />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/Register">Register</Link> |{" "}
+        <Link to="/Login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path = "/Register" element={<Register />} />
+        <Route path = "/Login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
